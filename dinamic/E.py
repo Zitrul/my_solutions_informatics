651dc9833bd789788198c088d076e7293f037f70
Zitrul <63922553+Zitrul@users.noreply.github.com>
2025-02-05 16:09:57 +0300

    Update E.py

--- dinamic/E.py
+++ dinamic/E.py
@@ -1,17 +1,63 @@
-N, M = map(int, input().split())
-m = list(map(int,input().split()))
-c = list(map(int,input().split()))
-l = []
-for i in range(N+1):
-    l.append([])
-    for j in range(M+1):
-        l[i].append(0)
-for i in range(1,N+1):
-    for j in range(1,M+1):
-        if j >= m[i-1]:
-            l[i][j] = max(l[i-1][j-m[i-1]] + c[i-1],l[i-1][j])
-        else:
-            l[i][j] = l[i - 1][j]
+n, m = map(int,input().split())
+d = []
+import sys
 
-#print(l)
-print(l[N][M])
+sys.setrecursionlimit(10 ** 6)
+used = []
+for i in range(n * m):
+    used.append(0)
+    d.append([])
+l = []
+for i in range(n):
+    x = input()
+    l.append(x)
+
+for i in range(n):
+    for j in range(m):
+        if l[i][j] == "#":
+            if i < n -1:
+                if l[i][j] == l[i + 1][j]:
+                    d[i * m + j].append((i + 1) * m + j)
+            if j < m - 1:
+                if l[i][j] == l[i][j + 1]:
+                    d[i * m + j].append(i * m + j + 1)
+            if j > 0:
+                if l[i][j] == l[i][j-1]:
+                    d[i * m + j].append(i * m + j-1)
+            if i > 0:
+                if l[i][j] == l[i-1][j]:
+                    d[i * m + j].append((i - 1) * m + j)
+        else:
+            used[i*m + j] =1
+v = d
+comp = []
+#print(v)
+#print(used)
+def dfs(i, o):
+    used[i] = 1
+    comp[o].append(i)
+    if i == 4:
+        for elm in v[i]:
+            #print("lskd;lfds;lkf;ldskf;lskf;lk;lka;lfkdsa;fasfkafadskfasd",elm, used[5])
+            if used[elm] != 1:
+                dfs(elm, o)
+    else:
+        for elm in v[i]:
+            if used[elm] != 1:
+                dfs(elm, o)
+    return 0
+
+
+
+kol_vo = 0
+sumka = 0
+while sum(used) != n*m:
+    kol_vo += 1
+    comp.append([])
+
+    start = used.index(0)
+    #print(start)
+    dfs(start, kol_vo-1)
+    #print(comp)
+    #print(used)
+print(kol_vo,end = "")
