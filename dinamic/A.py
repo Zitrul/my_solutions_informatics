cc51accf55cf1e1f824048469775fcf4856c4c97
Zitrul <63922553+Zitrul@users.noreply.github.com>
2025-02-05 16:07:19 +0300

    Update A.py

--- dinamic/A.py
+++ dinamic/A.py
@@ -1,6 +1,21 @@
-n = int(input())
-l1 = 1
-l2 = 1
+s = 0
+n,s = map(int,input().split())
+used = []
+v = []
 for i in range(n):
-    l1, l2 = l2, ( l1 + l2 ) % 10
-print(l1)
+    used.append(0)
+    v.append([])
+    l = list(map(int,input().split()))
+    for j in range(n):
+        if l[j]:
+            #print(l, j , l[j])
+            v[i].append(j)
+#print(v)
+def dfs(i):
+    used[i] = 1
+    for j in v[i]:
+        if used[j] != 1:
+            dfs(j)
+    return 0
+dfs(s-1)
+print(sum(used))
