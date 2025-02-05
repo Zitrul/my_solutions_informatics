0ea9f8ad7334260240cdcd0e1b707f7fc3e294f5
Zitrul <63922553+Zitrul@users.noreply.github.com>
2025-02-05 16:10:33 +0300

    Update F.py

--- dinamic/F.py
+++ dinamic/F.py
@@ -1,28 +1,59 @@
-s = int(input())
-N = int(input())
-A = list(map(int, input().split()))
-M = int(input())
-Q = list(map(int, input().split()))
-for i in range(M):
-    A.append(-Q[i])
-K = 10001
-F = [0] * (K + 1)
-F[0] = 1
-pref = [0] * (K + 1)
-for i in range(len(A)):
-    F_new = F[:]
-    for k in range(1, K + 1):
-        if 0 <= k - A[i] <= K and F[k - A[i]] == 1:
-            F_new[k] = 1
-            if pref[k] == 0:
-                pref[k] = A[i]
-    F = F_new
-if F[s] == 1:
-    while s != 0:
-        if pref[s] > 0:
-            print("+", pref[s], sep="", end=" ")
-        else:
-            print(pref[s], end=" ")
-        s -= pref[s]
-else:
-    print("Impossible")
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int MAXN = 1000000; // Adjust as needed for recursion limit
+vector<int> used;
+vector<vector<int>> v;
+vector<vector<int>> comp;
+
+void dfs(int i, int o) {
+    used[i] = 1;
+    comp[o].push_back(i);
+    for (int j : v[i]) {
+        if (used[j] != 1) {
+            dfs(j, o);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    used.resize(n, 0);
+    v.resize(n);
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        v[a - 1].push_back(b - 1);
+        v[b - 1].push_back(a - 1);
+    }
+
+    int kol_vo = 0;
+    int sumka = 0;
+
+    while (sumka != n) {
+        kol_vo++;
+        comp.push_back({});
+        int start = find(used.begin(), used.end(), 0) - used.begin();
+        dfs(start, kol_vo - 1);
+        sumka += comp[kol_vo - 1].size();
+    }
+
+    cout << kol_vo;
+    for (int i = 0; i < kol_vo; i++) {
+        cout << "\n" << comp[i].size() << "\n";
+        for (int elm : comp[i]) {
+            cout<< elm + 1 << " ";
+        }
+    }
+
+    return 0;
+}
